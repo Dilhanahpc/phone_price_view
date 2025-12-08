35f73899d0468c449386c0181580302540c26ce4
Chandupa Dilhana <chandupadilhan04@gmail.com>
2025-12-08 11:26:33 +0530

    Configure GitHub Actions workflow for Pages deployment

--- phone_price_frontend/vite.config.js
+++ phone_price_frontend/vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: '/phone_price_view/', // GitHub Pages repository name
+  base: process.env.NODE_ENV === 'production' ? '/phone_price_view/' : '/',
 })
